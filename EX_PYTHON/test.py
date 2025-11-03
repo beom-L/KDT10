70f06af7c973656a01a657a7cfc7709650002c46
Lee SeoBeom <eorlf421@yu.ac.kr>
2025-11-03 22:39:02 +0900

    new file:   EX_DATA_VS/DATA/stock_price.xlsx 	new file:   EX_DATA_VS/DATA/stock_valuation.xlsx 	modified:   EX_DATA_VS/DAY1030/ex_groupby_autompg.ipynb 	new file:   EX_DATA_VS/DAY1030/ex_mat_line.ipynb 	new file:   EX_DATA_VS/DAY1030/ex_mat_line_test.ipynb 	new file:   EX_DATA_VS/DAY1030/ex_mat_rc.ipynb 	new file:   "EX_DATA_VS/DAY1031/ex_func_mapping_\353\213\244\353\252\273\354\240\201\354\235\214.ipynb" 	new file:   EX_DATA_VS/DAY1031/ex_index.ipynb 	new file:   EX_DATA_VS/DAY1031/ex_index_multi_col.ipynb 	new file:   EX_DATA_VS/DAY1031/ex_index_multi_row.ipynb 	new file:   EX_DATA_VS/DAY1103/ex_plot_area.ipynb 	new file:   EX_DATA_VS/DAY1103/ex_plot_bar.ipynb 	new file:   EX_DATA_VS/DAY1103/ex_plot_common.ipynb 	new file:   EX_DATA_VS/DAY1103/ex_plot_line.ipynb 	new file:   EX_DATA_VS/DAY1103/ex_plot_scatter.ipynb 	new file:   "EX_DATA_VS/WORK/WORK_1030_\354\235\264\354\204\234\353\262\224.ipynb" 	new file:   "EX_DATA_VS/WORK/WORK_1031_\354\235\264\354\204\234\353\262\224.ipynb" 	new file:   "EX_DATA_VS/WORK/WORK_1103_\354\235\264\354\204\234\353\262\224.ipynb" 	modified:   EX_PYTHON/test.py

--- EX_PYTHON/test.py
+++ EX_PYTHON/test.py
@@ -4,16 +4,15 @@
 ## ?
 ## !
 ## *
+## =======================================
+def solution(strArr):
+    answer = [ len(s) for s in strArr ]
+    result = {}
+    for a in answer:
+        if a in result:
+            result[a] += 1
+        else:
+            result[a] = 1
+    return max(result.values())
 
-arr = [3, 2, 4, 1, 3]
-flag = [True, False, True, False, False]
-
-x = []
-
-for i in zip(arr,flag):
-    if i[1]:    ## True라면
-        for a in range(arr[0]*2):
-            x.append(arr[0])
-    else:
-        for a in range(arr[0]):
-            x.pop()
+print(solution(["a","bc","d","efg","hi"]))  # 2
