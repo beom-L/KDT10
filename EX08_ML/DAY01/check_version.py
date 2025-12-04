5e1b00df08b5e6835938b4b425b6bef23cc9e476
Lee SeoBeom <eorlf421@yu.ac.kr>
2025-12-04 16:55:18 +0900

    new file:   EX07_STATISTICS/DATA/ns_202104.csv 	new file:   EX07_STATISTICS/DAY01/ex_01_LE.ipynb 	new file:   EX07_STATISTICS/DAY01/ex_01_mean.ipynb 	new file:   EX07_STATISTICS/DAY01/ex_02_LE.ipynb 	new file:   EX07_STATISTICS/DAY02/ex02_qqplot.ipynb 	new file:   EX07_STATISTICS/DAY02/ex03_qqplot.ipynb 	new file:   EX07_STATISTICS/DAY02/ex_02_distribution.ipynb 	new file:   EX07_STATISTICS/DAY02/ex_ddof.ipynb 	new file:   EX07_STATISTICS/DAY02/test.ipynb 	new file:   EX07_STATISTICS/DAY03/ex02_kendalltau.ipynb 	new file:   EX07_STATISTICS/DAY03/ex03_point_biserial.ipynb 	new file:   EX07_STATISTICS/DAY03/ex04_chi.ipynb 	new file:   EX07_STATISTICS/DAY03/ex_probability_01.ipynb 	new file:   EX07_STATISTICS/DAY03/ex_probability_02.ipynb 	new file:   EX07_STATISTICS/DAY03/ex_probability_03.ipynb 	new file:   EX07_STATISTICS/DAY03/ex_probability_04.ipynb 	new file:   EX07_STATISTICS/DAY03/ex_probability_05.ipynb 	new file:   EX07_STATISTICS/DAY03/ex_recommand.ipynb 	new file:   EX07_STATISTICS/DAY03/ex_spearman_01.ipynb 	new file:   "EX07_STATISTICS/DAY03/ex_spearman_02_\353\247\210\354\247\200\353\247\211\353\266\200\353\266\204 \355\225\204\352\270\260 \353\215\234\355\225\250.ipynb" 	new file:   EX07_STATISTICS/DAY03/ex_survey.ipynb 	new file:   EX07_STATISTICS/DAY04/ex01_E_Var.ipynb 	new file:   EX07_STATISTICS/DAY04/ex02_discrete_distribution.ipynb 	new file:   EX07_STATISTICS/DAY04/ex04_pmf_pdf.ipynb 	new file:   EX07_STATISTICS/DAY04/ex05_continuous_distribution.ipynb 	new file:   EX07_STATISTICS/DAY04/ex06_continuous_distribution.ipynb 	new file:   EX07_STATISTICS/DAY04/ex07_continuous_distribution.ipynb 	new file:   "EX07_STATISTICS/EX/EXAM_STATS_1204_PM02_\354\235\264\354\204\234\353\262\224.ipynb" 	new file:   EX07_STATISTICS/EX/titanic.csv 	new file:   EX07_STATISTICS/EX/work_1203_descriptive_stats_answer.ipynb 	new file:   EX07_STATISTICS/EX/work_1203_descriptive_stats_exam.ipynb 	new file:   EX07_STATISTICS/EX/work_1203_probability_events_answer.ipynb 	new file:   EX07_STATISTICS/EX/work_1203_probability_events_exam.ipynb 	new file:   "EX07_STATISTICS/WORK/DAY03_work_1201_\354\235\264\354\204\234\353\262\224.ipynb" 	new file:   EX07_STATISTICS/WORK/ns_book4.csv 	new file:   EX07_STATISTICS/WORK/ns_book6.csv 	new file:   EX07_STATISTICS/WORK/ns_book7.csv 	new file:   "EX07_STATISTICS/WORK/work_1127_\354\235\264\354\204\234\353\262\224.ipynb" 	new file:   "EX07_STATISTICS/WORK/work_1128_\354\235\264\354\204\234\353\262\224.ipynb" 	new file:   "EX07_STATISTICS/WORK/work_1202_\354\235\264\354\204\234\353\262\224.ipynb" 	new file:   EX08_ML/DATA/auto_mpg.csv 	new file:   EX08_ML/DATA/fish.csv 	new file:   EX08_ML/DATA/housing.csv 	new file:   EX08_ML/DATA/housing_title_column.txt 	new file:   EX08_ML/DATA/iris.csv 	new file:   EX08_ML/DATA/mushrooms.csv 	new file:   EX08_ML/DATA/titanic.csv 	new file:   EX08_ML/DATA/titanic_train.csv 	new file:   EX08_ML/DAY01/check_version.py 	new file:   EX08_ML/DAY01/ex01_fish.ipynb 	new file:   EX08_ML/DAY01/ex02_fish.ipynb 	new file:   EX08_ML/DAY01/ex03_fish.ipynb 	new file:   "EX08_ML/WORK/work_ml_1203_\354\235\264\354\204\234\353\262\224.ipynb"

---
+++ EX08_ML/DAY01/check_version.py
@@ -0,0 +1,15 @@
+## -------------------------------------------------------
+## ML 가상환경 설치 패키지 버전 확인
+## -------------------------------------------------------
+import pandas as pd
+import sklearn as sk
+import numpy as np
+import matplotlib as ma
+import seaborn as sns
+
+## 설치 패키지별 버전 출력
+print(f'panda         v.{pd.__version__}')
+print(f'panda         v.{sk.__version__}')
+print(f'numpy         v.{np.__version__}')
+print(f'matplotlib    v.{ma.__version__}')
+print(f'seaborn       v.{sns.__version__}')
